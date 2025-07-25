d34dbd94dc679753cf9f6f646261bd3051e563fa
Ishan <ishan.sandhu3121@gmail.com>
2025-07-25 01:45:03 -0700

    hero page groundwork

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,15 @@ import Room from './Room'
 import Home from './home/Home'
 import EnterRoom from './enterRoom/EnterRoom'
 import CollabRoom from './collab/collabRoom'
+import Hero from './website/Hero'
 
 function App() {
   return (
     <Router>
       <Toaster position="top-right" />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Hero />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/generate-room" element={<Room />} />
         <Route path="/enter-room/:roomId" element={<EnterRoom />} />
         <Route path="/collab-room/:roomId" element={<CollabRoom />} />
