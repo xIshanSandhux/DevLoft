be5908ed91aafd6b137223095e9c5e1233d47ac4
Ishan <ishan.sandhu3121@gmail.com>
2025-07-25 16:04:21 -0700

    updated the landing hero page

--- client/src/website/Hero.jsx
+++ client/src/website/Hero.jsx
@@ -3,65 +3,82 @@ import HyperSpeed from './helper/HyperSpeed';
 
 export default function Hero() {
     return (
+        <div 
+            style={{
+                width: '100%', 
+                height: '100vh', 
+                position: 'relative',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: '0.5rem'
+            }}
+        >
+            <div className="heading-text">
+                Welcome to DevLoft
+            </div>
+            
+            <div 
+                className="sub-heading-text" 
+                style={{
+                    marginTop: '-1.5rem'
+                }}
+            >
+                A Collaborative Web IDE for Developers
+            </div> 
+
+            <button 
+                onClick={() => {
+                    window.location.href = '/generate-room'
+                }}
+                className="hero-button"
+                style={{
+                    marginTop: '2rem',
+                }}
+            >
+                Get Started
+            </button>
         
-        <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
-             <div
-    style={{
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      zIndex: 2,
-      width: '100%',
-      textAlign: 'center',
-      fontSize: '5rem',
-      fontWeight: 'bold',
-      color: 'white',
-      textShadow: '0 0 10px rgb(255, 11, 11)',
-    }}
-  >
-    Welcome to DevLoft
-  </div>
-        <HyperSpeed
-  effectOptions={{
-    onSpeedUp: () => { },
-    onSlowDown: () => { },
-    distortion: 'turbulentDistortion',
-    length: 400,
-    roadWidth: 10,
-    islandWidth: 2,
-    lanesPerRoad: 20,
-    fov: 90,
-    fovSpeedUp: 150,
-    speedUp: 2,
-    carLightsFade: 0.4,
-    totalSideLightSticks: 100,
-    lightPairsPerRoadWay: 50,
-    shoulderLinesWidthPercentage: 0.05,
-    brokenLinesWidthPercentage: 0.1,
-    brokenLinesLengthPercentage: 0.5,
-    lightStickWidth: [0.12, 0.5],
-    lightStickHeight: [1.3, 1.7],
-    movingAwaySpeed: [60, 80],
-    movingCloserSpeed: [-120, -160],
-    carLightsLength: [400 * 0.03, 400 * 0.2],
-    carLightsRadius: [0.05, 0.14],
-    carWidthPercentage: [0.3, 0.5],
-    carShiftX: [-0.8, 0.8],
-    carFloorSeparation: [0, 5],
-    colors: {
-      roadColor: 0x080808,
-      islandColor: 0x0a0a0a,
-      background: 0x000000,
-      shoulderLines: 0xFFFFFF,
-      brokenLines: 0xFFFFFF,
-      leftCars: [0xD856BF, 0x6750A2, 0xC247AC],
-      rightCars: [0x03B3C3, 0x0E5EA5, 0x324555],
-      sticks: 0x03B3C3,
-    }
-  }}
-/>
-    </div>
-       
+            <HyperSpeed
+                effectOptions={{
+                    onSpeedUp: () => { },
+                    onSlowDown: () => { },
+                    distortion: 'turbulentDistortion',
+                    length: 400,
+                    roadWidth: 10,
+                    islandWidth: 2,
+                    lanesPerRoad: 20,
+                    fov: 90,
+                    fovSpeedUp: 150,
+                    speedUp: 2,
+                    carLightsFade: 0.4,
+                    totalSideLightSticks: 100,
+                    lightPairsPerRoadWay: 50,
+                    shoulderLinesWidthPercentage: 0.05,
+                    brokenLinesWidthPercentage: 0.1,
+                    brokenLinesLengthPercentage: 0.5,
+                    lightStickWidth: [0.12, 0.5],
+                    lightStickHeight: [1.3, 1.7],
+                    movingAwaySpeed: [60, 80],
+                    movingCloserSpeed: [-120, -160],
+                    carLightsLength: [400 * 0.03, 400 * 0.2],
+                    carLightsRadius: [0.05, 0.14],
+                    carWidthPercentage: [0.3, 0.5],
+                    carShiftX: [-0.8, 0.8],
+                    carFloorSeparation: [0, 5],
+                    colors: {
+                        roadColor: 0x080808,
+                        islandColor: 0x0a0a0a,
+                        background: 0x000000,
+                        shoulderLines: 0xFFFFFF,
+                        brokenLines: 0xFFFFFF,
+                        leftCars: [0xD856BF, 0x6750A2, 0xC247AC],
+                        rightCars: [0x03B3C3, 0x0E5EA5, 0x324555],
+                        sticks: 0x03B3C3,
+                    }
+                }}
+            />
+        </div>
     )
 }
